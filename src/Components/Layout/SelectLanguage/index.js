818669d51d98a5ef30b0c928a6ca1eaed1377291
texidev <tevixdev@gmail.com>
2021-12-21 23:21:00 -0300

    fixed flag and modified sw

--- src/Components/Layout/SelectLanguage/index.js
+++ src/Components/Layout/SelectLanguage/index.js
@@ -11,7 +11,7 @@ import flagEnglish from 'Assets/Images/united_flag.ico'
 export const SelectLanguage = () => {
   const { t, i18n } = useTranslation('selectLanguage')
   const language = localStorage.getItem('language')
-  const flag = language === 'en' ? flagSpanish : flagEnglish
+  const flag = (language === 'es' || language === null) ? flagEnglish : flagSpanish
 
   const changeLanguage = ({ language }) => {
     if (language === null || language === 'es') {
